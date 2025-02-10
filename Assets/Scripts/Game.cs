f3a0ead0ef27e56105caad3b6db51d4dea1719be
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-02-10 23:58:12 +0300

    White Team Pieces Setup

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,9 +6,23 @@ public class Game : MonoBehaviour
 {
     public GameObject piecePrefab;
 
-    // Start is called before the first frame update
+    //Positions and teams
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] playerWhite = new GameObject[16];
+    private GameObject[] playerBlack = new GameObject[16];
+
+    private string currentPlayer = "white";
+
+    private bool gameOver = false;
+
     void Start()
     {
-        Instantiate(piecePrefab, new Vector3(0, 0, -1), Quaternion.identity);
+        playerWhite = new GameObject[] {
+            Create("white_rook",0,0), Create("white_knight",1,0),
+            Create("white_bishop",2,0), Create("white_queen",3,0), Create("white_king",3,0),
+            Create("white_bishop",5,0), Create("white_knight",6,0), Create("white_rook",7,0),
+            Create("white_pawn",0,1), Create("white_pawn",1,1), Create("white_pawn",2,1),
+            Create("white_pawn",3,1), Create("white_pawn",4,1), Create("white_pawn",5,1),
+            Create("white_pawn",6,1), Create("white_pawn",7,1) };
     }
 }
